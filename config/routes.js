7cc3b2fee152b17dd398e04876427fd0d30f432e
Hunter315 <hunter.mckay.smith@gmail.com>
2018-11-30 09:51:58 -0700

    login and register working

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,9 @@
 const axios = require('axios');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('knex')(require('../knexfile').development);
 
+const jwtKey = require('../_secrets/keys').jwtKey;
 const { authenticate } = require('./middlewares');
 
 module.exports = server => {
@@ -8,13 +12,60 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+function generateJWT(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const secret = jwtKey;
+  const options = {
+    expiresIn: '1h',
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
 function register(req, res) {
   // implement user registration
-}
+  const creds = req.body;
+ //requires both username and password to be input
+ 
+  //hash that password
+  const hash = bcrypt.hashSync(creds.password, 6);
+
+  //override pass with my new hash
+  creds.password = hash;
+
+  //save that info to my db
+  db('users')
+  .insert(creds)
+  .then(ids => {
+      res.status(201).json({message: "Registration Successful"}) //if reg doesnt work, try removing ids
+  })
+  .catch(err => res.status(400).json({error: "Unable to Register", err}));
+  
+  }
+
+
 
 function login(req, res) {
   // implement user login
-}
+  const creds = req.body;
+
+    db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+        if(user && bcrypt.compareSync(creds.password, user.password)) {
+            const token = generateJWT(user);
+            res.status(200).json(token); //only including token for dev purposes
+        } else {
+            res.status(401).json({message: "You shall not pass!"});
+        }
+    })
+    .catch(err => res.status(401).json({message: "Error logging you in"}));
+};
 
 function getJokes(req, res) {
   axios
@@ -25,6 +76,6 @@ function getJokes(req, res) {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: 'Error Fetching Jokes'});
     });
 }
